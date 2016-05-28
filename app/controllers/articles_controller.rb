2a4b7d847b4d20f0c0924ad2e13d08ea16bfdf1c
Jiazhi Guo <jerrykuo7727@gmail.com>
2016-05-28 19:58:41 +0800

    finish articles.create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+	include ArticlesHelper
 	def index
 		@articles = Article.all
 	end
@@ -9,5 +10,12 @@ class ArticlesController < ApplicationController
 
 	def new
 		@article = Article.new
+	end
+
+	def create
+		@article = Article.new(article_params)
+		@article.save
+
+		redirect_to article_path(@article)
 	end
 end
