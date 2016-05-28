38d5cc308dcefe5c946deb1f2c38e0dc81bf8988
Jiazhi Guo <jerrykuo7727@gmail.com>
2016-05-29 05:50:19 +0800

    fix tag uniqueness

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,14 +7,12 @@ class Article < ActiveRecord::Base
     validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
     def tag_list
-    	self.tags.collect do |tag|
-    		tag.name
-    	end.join(", ")
+        tags.join(", ")
     end
 
     def tag_list=(tags_string)
-  		tag_names = tags_string.split(",").collect{|s| s.strip.downcase}.uniq
-  		new_or_found_tags = tag_names.collect {|name| tag = Tag.find_or_create_by(name: name)}
-    	self.tags = new_or_found_tags
+        tag_names = tags_string.split(",").collect{|s| s.strip.downcase}.uniq
+        new_or_found_tags = tag_names.collect {|name| Tag.find_or_create_by(name: name)}
+        self.tags = new_or_found_tags
     end
 end
